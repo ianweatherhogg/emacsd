be6aac094ecb4855a818e9fdbece56d10cd03fef
Ian Weatherhogg <ian@ianweatherhogg.com>
2014-08-19 16:22:11 +0100

    disabled electric-pair-mode

--- init.el
+++ init.el
@@ -371,7 +371,7 @@
  '(custom-enabled-themes (quote (tango-plus)))
  '(custom-safe-themes (quote ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "f5e9f66da69f504cb61aacedeb8284d8f38f2e6f835fd658cac5f0ad5d924549" default)))
  '(delete-by-moving-to-trash t)
- '(electric-pair-mode t)
+ '(electric-pair-mode nil)
  '(history-length 10000)
  '(indent-tabs-mode nil)
  '(blink-cursor-mode nil)
