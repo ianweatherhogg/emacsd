250f7ebf5b3ab481790bdeff5be6a2d4803c9da7
Ian Weatherhogg <ian@ianweatherhogg.com>
2015-01-13 16:53:05 +0000

    updating init changes which I forgot

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (setq user-full-name "Ian Weatherhogg")
 (setq user-mail-address "ian@ianweatherhogg.com")
 
-(require 'cask (expand-file-name "emacs-cask/cask.el" user-emacs-directory))
+(require 'cask)
+;; (require 'cask (expand-file-name "emacs-cask/cask.el" user-emacs-directory))
 (cask-initialize)
 
 (require 'use-package)
